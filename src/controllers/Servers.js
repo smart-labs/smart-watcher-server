6f3c28c80879d0c5e8d44537939fb1a1919776f5
Jonatan Lins <jonatanpinheiro99@gmail.com>
2019-11-20 00:04:21 -0300

    mudanças

--- src/controllers/Servers.js
+++ src/controllers/Servers.js
@@ -10,23 +10,30 @@ class ServersController {
   static async create(req, res) {
     const result = await Server.create(req.body);
 
+    req.io.emit("servers/create", result);
+
     res.send(result);
   }
 
   static async show(req, res) {
-    const result = await Server.findById(req.query.id);
+    console.log(req.params);
+    const result = await Server.findById(req.params.id);
 
     res.send(result);
   }
 
   static async update(req, res) {
-    const result = await Server.findByIdAndUpdate(req.query.id);
+    const result = await Server.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+
+    req.io.emit("servers/update", result);
 
     res.send(result);
   }
 
   static async destroy(req, res) {
-    const result = await Server.findByIdAndDelete(req.query.id);
+    const result = await Server.findByIdAndDelete(req.params.id);
 
     res.send(result);
   }
